2841ed8e3c2e4fdeb28fb7817b492417aeb93b83
mrk-test <mrk012@yahoo.com>
2017-07-16 18:24:40 +0800

    change packaging to war

--- src/main/java/com/example/demo/SimpleWebApplication.java
+++ src/main/java/com/example/demo/SimpleWebApplication.java
@@ -2,15 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SimpleWebApplication {
+public class SimpleWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleWebApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SimpleWebApplication.class);
 	}
 
 	@GetMapping("/hello")
